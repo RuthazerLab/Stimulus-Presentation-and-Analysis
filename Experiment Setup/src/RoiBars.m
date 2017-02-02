0debf955f7951761ac6f3e9925dbf3a38c6244fd
niklasbrake <niklas.brake@mail.mcgill.ca>
2017-02-02 12:33:16 -0500

    Roi Bars
    Updated RunExperiment to show bars to map receptive field sizes. Also
    added ability to set background an stimulus shades.

--- Experiment Setup/src/RoiBars.m
+++ Experiment Setup/src/RoiBars.m
@@ -1,36 +1,39 @@
-function I = square(varargin)   % Called from stimulusFull.m
+function I = RoiBars(varargin)
 
-  % Returns image of a square in specified location.
+% Returns an image with properties specified in varargin.
+% Displays a single square either brighter or darker than
+% a grey background.
 
   variables = varargin{1};
 
   gridnum = variables{1}; % Position of square
   ssiz = variables{2};    % Size of display area
-  num = variables{3};     % Number of squares in display area
+  num = variables{3};     % Number of bars in display area
   height = variables{4};  % Height of image
   width = variables{5};   % Width of image
   buff = variables{6};    % Offset from bottom of screen
   Sign = variables{7};    % Darker or lighter than background
   GreyBackground = variables{8};  % Weight of background
   PlusMinusDifference = variables{9};   % Absolute difference between light/dark and background
+  isVert = variables{10};
 
   Colour = GreyBackground + Sign*PlusMinusDifference;
   I = ones(height,width) * GreyBackground;
 
-  pos = gridnum -1;
+  if(gridnum == 0)
+    return
+  end
+
+  pos = gridnum;
   siz = ssiz/num;
-  
 
-  a1 = height-buff-ssiz;
-  b1 = height-buff;
-  a2 = floor((width -ssiz)/2);
-  b2 = floor((width + ssiz)/2);
+  a1 = height-buff-ssiz;	% Top Edge
+  b1 = height-buff;			% Bottom Edge
+  a2 = (width -ssiz)/2;		% Left Side
+  b2 = (width + ssiz)/2;	% Right Side
 
-% Loops through all pixels in display area
-  for i = a1:b1
-    for j = a2:b2
-      if(mod(pos,num)*siz+a1 < i && i <= (mod(pos,num)+1)*siz+a1 && floor(pos/num)*siz+a2 < j && j <= (floor(pos/num)+1)*siz+a2)
-        I(i,j) = Colour;
-      end
-    end
+  if(isVert)
+  	I(a1+siz*(pos-1):a1+siz*pos,a2:b2) = Colour;
+  else
+  	I(a1:b1,a2+siz*(pos-1):a2+siz*pos) = Colour;
   end
